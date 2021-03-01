7b420f7efe9467212a7102b421ba81b1622749eb
tejas <tejas1188245@gmail.com>
2021-03-01 16:13:08 +0530

    Add Card Component Object

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -2,19 +2,29 @@
 import React from 'react';
 
 class Cartitem  extends React.Component{
-
+    constructor(){
+        super();
+        this.state = {
+           price:999,
+           title:'Mobile Phone',
+           qty:1
+        }
+    }
     render (){
+        const {title,price,qty} =this.state;
         return(
-
          <div className='cart-item'>
             <div className='left-block'>
-                <img style={styles.images}/>
+                <img style={styles.images} src="" alt=""/>
             </div>
             <div className='right-block'>
-                <div style={{fontSize:25}}>Phone</div>
-                <div style={{color:'#777'}}>Rs 999</div>
-                <div style={{color:'#777'}}>QTY: 1</div>
+                <div style={{fontSize:25}}>{title}</div>
+                <div style={{color:'#777'}}>RS:{price}</div>
+                <div style={{color:'#777'}}>QTY:{qty}</div>
                 <div className='cart-item-actions'>
+                <img  alt="increase" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1614594803~hmac=27e63ad413131423aa99a9521dc2b714"/>
+                <img  alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1614594653~hmac=db582325df1b2a63fac0ac48e23e5ad2"/>
+                <img  alt="delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/565/565491.svg?token=exp=1614594841~hmac=b3ced74b4ed5d87a50da1b03bfb0ce52"/>
               </div>
             </div>
          </div>
